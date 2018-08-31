a6b7fecd29543a28dc338c86f7e9ae81c9b671a9
Ivan Vakulov <1604881@gmail.com>
2018-08-31 12:38:13 +0300

    recommit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+// var tinyPngWebpackPlugin = require('tinypng-webpack-plugin');
+
 module.exports = {
     baseUrl: process.env.NODE_ENV === 'production'
-        ? '/prod/'
-        : '/'
+        ? ''
+        : '/',
 }
+
+// module.exports = {
+//     plugins: [
+//         new tinyPngWebpackPlugin({
+//             key:"VBDkBCpEXLRet4VLaS4IBUGdwNi6YQL4",//can be Array, eg:['your key 1','your key 2'....]
+//             ext: ['png', 'jpeg', 'jpg'],//img ext name
+//         })
+//     ]
+// }
